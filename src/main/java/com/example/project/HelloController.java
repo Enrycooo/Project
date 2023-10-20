004a6ecaf4dc3bd006d4df1f4c7c1525a892143a
Raphaël D <daniel.rapahel@hotmail.fr>
2023-10-20 11:05:22 +0200

    Mise en place de changement d'interface si combobox=enseignant

--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -1,17 +1,44 @@
 package com.example.project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ComboBox<String> choiceBox;
+    @FXML
+    private Label lblNombreCours;
+    @FXML
+    private Label lblSpecialite;
+    @FXML
+    private Label txtSpecialite;
+    @FXML
+    private Label ;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("C'est toi le plus beau habibi");
     }
-    protected void onComboBoxClick() {
 
+    @FXML
+    protected void onComboBoxClick() {
+        String selectedValue = choiceBox.getValue();
+
+        if (selectedValue != null) {
+            if (selectedValue.equals("Enseignant")) {
+                lblNombreCours.setVisible(true);
+                lblSpecialite.setVisible(true);
+                txtSpecialite.setVisible(true);
+                txtNombrecours.setVisible(true);
+            } else {
+                lblNombreCours.setVisible(false);
+                lblSpecialite.setVisible(false);
+                txtSpecialite.setVisible(false);
+                txtNombrecours.setVisible(false);
+            }
+        }
     }
 }
